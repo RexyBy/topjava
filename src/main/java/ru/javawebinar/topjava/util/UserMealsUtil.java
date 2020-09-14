1461d4836a560398943465daf085a913761d8c31
SuDex <sudnishnikoff@gmail.com>
2020-09-14 20:34:00 +0300

    implementation by streams API (OPTIONAL 1)

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Collectors;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -21,10 +23,10 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
         );
 
-        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-        mealsTo.forEach(System.out::println);
+//       List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+//        mealsTo.forEach(System.out::println);
 
-//        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
@@ -47,8 +49,17 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO Implement by streams
-       return null;
+        Map<LocalDate, Integer> caloriesForEachDate = calculateCaloriesForEachDay(meals);
+
+        return meals.stream()
+                .filter(nextMeal -> TimeUtil.isBetweenHalfOpen(nextMeal.getDateTime().toLocalTime(), startTime, endTime))
+                .map(nextMeal ->
+                        new UserMealWithExcess(
+                                nextMeal.getDateTime(),
+                                nextMeal.getDescription(),
+                                nextMeal.getCalories(),
+                                caloriesForEachDate.getOrDefault(nextMeal.getDateTime().toLocalDate(), 0) > caloriesPerDay))
+                .collect(Collectors.toList());
     }
 
     private static Map<LocalDate, Integer> calculateCaloriesForEachDay(List<UserMeal> meals) {
