0a92bdbd49c93f21186c6bddc312a1b70b160a73
SuDex <sudnishnikoff@gmail.com>
2020-10-19 15:39:46 +0300

    Duplicate meal create test added

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
@@ -89,6 +90,13 @@ public class MealServiceTest {
         newMeal.setId(newId);
         assertMatch(created, newMeal);
         assertMatch(service.get(newId, USER_ID), newMeal);
+    }
+
+    @Test
+    public void duplicateMealCreate() throws Exception {
+        Meal newMeal = getNew();
+        newMeal.setDateTime(userBreakfast.getDateTime());
+        assertThrows(DataAccessException.class, () -> service.create(newMeal, USER_ID));
     }
 
     @Test
