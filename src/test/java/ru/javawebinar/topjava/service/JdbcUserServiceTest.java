68f31413e133baf2e5517cfe073837b8265ef6fc
SuDex <sudnishnikoff@gmail.com>
2020-11-02 20:41:02 +0300

    Tests refactored and servlet fixed

--- src/test/java/ru/javawebinar/topjava/service/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/JdbcUserServiceTest.java
@@ -5,58 +5,5 @@ import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
 
 @ActiveProfiles(Profiles.JDBC)
-public class JdbcUserServiceTest extends UserServiceTest {
-    @Override
-    @Test
-    public void create() {
-        super.create();
-    }
-
-    @Override
-    @Test
-    public void duplicateMailCreate() {
-        super.duplicateMailCreate();
-    }
-
-    @Override
-    @Test
-    public void delete() {
-        super.delete();
-    }
-
-    @Override
-    @Test
-    public void deleteNotFound() {
-        super.deleteNotFound();
-    }
-
-    @Override
-    @Test
-    public void get() {
-        super.get();
-    }
-
-    @Override
-    @Test
-    public void getNotFound() {
-        super.getNotFound();
-    }
-
-    @Override
-    @Test
-    public void getByEmail() {
-        super.getByEmail();
-    }
-
-    @Override
-    @Test
-    public void update() {
-        super.update();
-    }
-
-    @Override
-    @Test
-    public void getAll() {
-        super.getAll();
-    }
+public final class JdbcUserServiceTest extends UserServiceTest {
 }
