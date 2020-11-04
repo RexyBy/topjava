0ad264c7193979f28cdca15e4abc8a73b609756a
SuDex <sudnishnikoff@gmail.com>
2020-11-04 16:26:26 +0300

    SpringMain fixed

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -17,7 +17,13 @@ import java.util.List;
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 automatic resource management (ARM)
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml")) {
+        ConfigurableApplicationContext appCtx;
+        ClassPathXmlApplicationContext classPathContext = new ClassPathXmlApplicationContext();
+        classPathContext.setConfigLocations("spring/spring-app.xml", "spring/spring-db.xml");
+        classPathContext.getEnvironment().setActiveProfiles(Profiles.getActiveDbProfile(), Profiles.DATAJPA);
+        appCtx = classPathContext;
+        appCtx.refresh();
+        try (appCtx) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
